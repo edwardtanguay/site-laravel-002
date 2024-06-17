a2ea595dd18ceb73986f93cb6f71d8cbb0f1e76a
Edward Tanguay <edward@tanguay.info>
2024-06-17 18:45:27 +0200

    layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
-<h1>This is the home page</h1>
+@extends('layouts.base')
+
+@section('content')
+<h1 class="text-2xl text-red-500">This is the home page</h1>
 @if($userGroup == "admin")
 <p>Welcome to {{ $companyName }}.</p>
 @else
 <p>Please login for {{$companyName}}.</p>
 @endif
+@endsection
